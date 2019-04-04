61b6c40e526c15641376f1fa06c91b8a9220d317
Duong Tuan <duong.anh.tuanb@framgia.com>
2019-04-04 10:02:29 +0700

    Fix bug category screen

--- app/src/main/java/com/example/boylc/assmob403/adapter/GifsAdapter.java
+++ app/src/main/java/com/example/boylc/assmob403/adapter/GifsAdapter.java
@@ -1,7 +1,9 @@
 package com.example.boylc.assmob403.adapter;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -9,8 +11,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.example.boylc.assmob403.R;
 import com.example.boylc.assmob403.model.Gifs;
 import com.example.boylc.assmob403.uis.fragments.DetailGiftFragment;
@@ -27,12 +34,15 @@ public class GifsAdapter extends RecyclerView.Adapter<GifsAdapter.ViewHolder> {
 
         private TextView txtTotalView;
 
+        private ProgressBar mProgressBar;
+
 
         public ViewHolder(View itemView) {
             super(itemView);
             imgThumb = itemView.findViewById(R.id.img_thumb);
             txtTotalView = itemView.findViewById(R.id.txt_total_view);
             txtTotalLike = itemView.findViewById(R.id.txt_total_like);
+            mProgressBar = itemView.findViewById(R.id.progress_image);
             itemView.setOnClickListener(this);
         }
 
@@ -65,10 +75,27 @@ public class GifsAdapter extends RecyclerView.Adapter<GifsAdapter.ViewHolder> {
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull final ViewHolder holder, int position) {
         Gifs gifs = mGifs.get(position);
         Glide.with(mContext)
                 .load(gifs.getGifImage())
+                .listener(new RequestListener<Drawable>() {
+                    @Override
+                    public boolean onLoadFailed(@Nullable final GlideException e, final Object model,
+                            final Target<Drawable> target,
+                            final boolean isFirstResource) {
+                        holder.mProgressBar.setVisibility(View.GONE);
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(final Drawable resource, final Object model,
+                            final Target<Drawable> target,
+                            final DataSource dataSource, final boolean isFirstResource) {
+                        holder.mProgressBar.setVisibility(View.GONE);
+                        return false;
+                    }
+                })
                 .into(holder.imgThumb);
         holder.txtTotalView.setText(gifs.getTotalViews());
     }
